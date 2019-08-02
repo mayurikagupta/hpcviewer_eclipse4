cb16cd28123845fd0a99c7e87e4c1fe57b564c44
Mayurika Gupta <49951665+mayurikagupta@users.noreply.github.com>
2019-08-02 18:16:17 -0500

    Add files via upload

---
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/components/ViewerCoolBar.java
@@ -0,0 +1,22 @@
+package edu.rice.cs.hpcviewer.components;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ToolItem;
+
+/**
+ * 
+ * Base coolbar for all standard views
+ * this cool bar contains:
+ * - zoom: in/out
+ * - hot spot
+ * - metric: hide/show, name editing, derived metrics 
+ * - font: larger, smaller
+ */
+public class ViewerCoolBar 
+{
+	ToolItem tiZoomIn, tiZoomOut;
+
+	public ViewerCoolBar(Composite parent) {
+		
+	}
+}
